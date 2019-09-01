3fae5f9a2d9b1a9556fdf38c6f498b98fd81469b
Home\Richa <richard.noordam@gmail.com>
2019-09-01 09:18:49 -0700

    pics and code change- moisture sensor

--- ArduinoDigitalMoistureSensor/ArduinoDigitalMoistureSensor.ino
+++ ArduinoDigitalMoistureSensor/ArduinoDigitalMoistureSensor.ino
@@ -1,16 +1,23 @@
-#define SensorPin 2
-
+#define SensorPin 10
+#define LEDPin 13
 int val = 0;
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin (2400);
+  pinMode(SensorPin, INPUT);
+  pinMode(LEDPin, OUTPUT);
+  Serial.begin (9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  val = analogRead(A0);
-  Serial.write(val);
-      
+  if (digitalRead(SensorPin) == HIGH){
+    Serial.write("HIGH - Water Needed.");
+    digitalWrite(LEDPin, HIGH);
+  } else {
+    Serial.write("LOW - No Water Needed.");
+    digitalWrite(LEDPin, LOW);
+  }
+  Serial.write("\n");
   delay(1000);
 }
